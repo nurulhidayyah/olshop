d5c88c120629dcfba2b18c9930433022adf1b7cc
nurulhidayyah <nurulhidayyah45@gmail.com>
2022-07-31 01:26:54 +0700

    27. Update dan Delete Keranjang Belanja

--- application/controllers/Belanja.php
+++ application/controllers/Belanja.php
@@ -12,6 +12,14 @@ class Belanja extends CI_Controller
 
     public function index()
     {
+        if (empty($this->cart->contents())) {
+            redirect('home');
+        }
+        $data = array(
+            'title' => 'Keranjang Belanja',
+            'isi' => 'v_belanja'
+        );
+        $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
     }
 
     public function add()
@@ -26,5 +34,31 @@ class Belanja extends CI_Controller
         $this->cart->insert($data);
 
         redirect($redirect_page, 'refresh');
+    }
+
+    public function delete($rowid)
+    {
+        $this->cart->remove($rowid);
+        redirect('belanja');
+    }
+
+    public function update()
+    {
+        $i = 1;
+        foreach ($this->cart->contents() as $items) {
+            $data = array(
+                'rowid' => $items['rowid'],
+                'qty'   => $this->input->post($i . '[qty]')
+            );
+            $this->cart->update($data);
+            $i++;
+        }
+        redirect('belanja');
+    }
+
+    public function clear()
+    {
+        $this->cart->destroy();
+        redirect('belanja');
     }
 }
---
+++ application/views/v_belanja.php
@@ -0,0 +1,67 @@
+<div class="card card-solid">
+    <div class="card-body pb-0">
+        <div class="row">
+            <div class="col-lg-12">
+                <?php echo form_open('belanja/update'); ?>
+
+                <table class="table" cellpadding="6" cellspacing="1" style="width:100%">
+
+                    <tr>
+                        <th width="100px">QTY</th>
+                        <th>Nama Barang</th>
+                        <th style="text-align:right">Harga</th>
+                        <th style="text-align:right">Sub-Total</th>
+                        <th class="text-center">Action</th>
+                    </tr>
+
+                    <?php $i = 1; ?>
+
+                    <?php foreach ($this->cart->contents() as $items) : ?>
+
+                        <tr>
+                            <td>
+                                <?php
+                                echo form_input(array(
+                                    'name' => $i . '[qty]',
+                                    'value' => $items['qty'],
+                                    'maxlength' => '3',
+                                    'min' => '0',
+                                    'size' => '5',
+                                    'type' => 'number',
+                                    'class' => 'form-control'
+                                ));
+                                ?>
+                            </td>
+                            <td><?php echo $items['name']; ?></td>
+                            <td style="text-align:right">Rp. <?php echo number_format($items['price'], 0); ?></td>
+                            <td style="text-align:right">Rp. <?php echo number_format($items['subtotal'], 0); ?></td>
+                            <td class="text-center">
+                                <a href="<?= base_url('belanja/delete/' . $items['rowid']); ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+
+                        <?php $i++; ?>
+
+                    <?php endforeach; ?>
+
+                    <tr>
+                        <td colspan="2"> </td>
+                        <td class="right">
+                            <h3>Total</h3>
+                        </td>
+                        <td class="right">
+                            <h3>Rp. <?php echo number_format($this->cart->total(), 0); ?></h3>
+                        </td>
+                    </tr>
+
+                </table>
+
+                <button type="submit" class="btn btn-primary mb-3"><i class="fa fa-save"></i> Update Cart</button>
+                <a href="<?= base_url('belanja/clear'); ?>" class="btn btn-danger text-white mb-3"><i class="fa fa-recycle"></i> Clear Cart</a>
+                <a href="#" class="btn btn-success text-white mb-3"><i class="fa fa-check-square"></i> Checkout</a>
+
+                <?php form_close(); ?>
+            </div>
+        </div>
+    </div>
+</div>
