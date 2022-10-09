1f6e5058ddc43df6061c4ddaaa3e4c5053800dfa
nurulhidayyah <nurulhidayyah45@gmail.com>
2022-10-09 21:43:40 +0700

    39. Pesanan Saya Part 1

--- application/controllers/Pesanan_saya.php
+++ application/controllers/Pesanan_saya.php
@@ -1,18 +1,25 @@
-<?php 
+<?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class pesanan_saya extends CI_Controller {
+class pesanan_saya extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_transaksi');
+    }
 
     public function index()
     {
         $data = array(
             'title' => 'Pesanan Saya',
+            'belum_bayar' => $this->m_transaksi->belum_bayar(),
             'isi' => 'v_pesanan_saya'
         );
         $this->load->view('layout/v_wrapper_frontend', $data, FALSE);
     }
-
 }
 
 /* End of file pesanan_saya.php */
--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -1,16 +1,27 @@
-<?php 
+<?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class M_transaksi extends CI_Model {
+class M_transaksi extends CI_Model
+{
 
     public function simpan_transaksi($data)
     {
         $this->db->insert('tbl_transaksi', $data);
     }
+
     public function simpan_rinci_transaksi($data_rinci)
     {
         $this->db->insert('tbl_rinci_transaksi', $data_rinci);
     }
 
+    public function belum_bayar()
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_transaksi');
+        $this->db->where('status_bayar=0');
+        $this->db->where('id_pelanggan', $this->session->userdata('id_pelanggan'));
+        $this->db->order_by('id_transaksi', 'desc');
+        return $this->db->get()->result();
+    }
 }
