f48df78ae3b264a998afaaeaf217e423dc60b96c
nurulhidayyah <nurulhidayyah45@gmail.com>
2022-11-06 01:32:32 +0700

    41. Pesanan Masuk

---
+++ application/models/M_pesanan_masuk.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_pesanan_masuk extends CI_Model
+{
+
+    public function pesanan()
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_transaksi');
+        $this->db->where('status_order=0');
+        $this->db->order_by('id_transaksi', 'desc');
+        return $this->db->get()->result();
+    }
+}
+
+/* End of file M_pesanan_masuk.php */
